baa2444579b364425f6f7e301cc4c1946b200e83
Christopher Boyle <chrismjboyle@gmail.com>
2024-07-13 14:12:17 +0100

    Change to next state, if requested, after running state

--- src/state.rs
+++ src/state.rs
@@ -3,24 +3,51 @@ use json::JsonValue;
 use crate::expression::Expression;
 
 
+#[derive(Debug)]
+pub enum StateOp {
+    Stay,
+    ChangeTo(String),
+    End
+}
+
+
+impl StateOp {
+    pub fn from_str(s: &str) -> anyhow::Result<Self> {
+        let rv = if let Some(s) = s.strip_prefix("changeto ") {
+            Self::ChangeTo(s.to_string())
+        }
+        else {
+            match s {
+                "stay" => Self::Stay,
+                "end" => Self::End,
+                s => anyhow::bail!("unexpected stateop {s:?}")
+            }
+        };
+
+        Ok(rv)
+    }
+}
+
+
 pub struct State {
     /// Name of state which is the default successor to this one
-    default: String,
+    default_next: Option<String>,
 
     /// Expressions evaluated when this state is visited
     head: Vec<Expression>,
 
     /// List of condition expressions and associated expressions.
     /// When the condition expression is true, the associated body of expressions is run.
-    body: Vec<(Expression, Vec<Expression>)>,
+    body: Vec<(Expression, Vec<Expression>, StateOp)>,
 }
 
 impl State {
-    pub fn new(default: String, head: Vec<Expression>, body: Vec<(Expression, Vec<Expression>)>) -> Self {
-        Self { default, head, body }
+    pub fn new(default_next: Option<String>, head: Vec<Expression>, body: Vec<(Expression, Vec<Expression>, StateOp)>) -> Self {
+        Self { default_next, head, body }
     }
 
-    pub fn run(&self, i: &JsonValue, g: &mut JsonValue) -> anyhow::Result<JsonValue> {
+    // return some output if not ended
+    pub fn run(&self, i: &JsonValue, g: &mut JsonValue) -> anyhow::Result<(JsonValue, StateOp)> {
         let mut o = json::object! { };
 
         for expr in &self.head {
@@ -28,8 +55,5 @@ impl State {
         }
 
         todo!();
-
-
-        Ok(o)
     }
 }
--- src/state_machine.rs
+++ src/state_machine.rs
@@ -7,19 +7,23 @@ use std::io::Read;
 use json::JsonValue;
 
 use crate::expression::Expression;
-use crate::state::State;
+use crate::state::{State, StateOp};
 
 
 pub struct StateMachine {
+    // Might need RefCell here
     states: HashMap<String, State>,
-    current_state: String,
+    initial_state: String,
+    current_state: Option<String>, // if using RC<RefCell<>>, then can put the state directly here
+                                   // instead of the name...
     globals: JsonValue,
 }
 
 impl StateMachine {
-    pub fn new(states: HashMap<String, State>, current_state: String) -> Self {
+    pub fn new(states: HashMap<String, State>, initial_state: String) -> Self {
         let globals = json::object! { };
-        Self { states, current_state, globals }
+        let current_state = Some(initial_state.clone());
+        Self { states, initial_state, current_state, globals }
     }
 
     pub fn from_file<P: Into<PathBuf>>(path: P) -> anyhow::Result<Self> {
@@ -32,18 +36,26 @@ impl StateMachine {
 
     pub fn from_src(src: &str) -> anyhow::Result<Self> {
         let json = json::parse(src)?;
-        if !json.is_object() {
-            return Err(anyhow::anyhow!("json expected to be object"));
+        if !json.is_array() {
+            return Err(anyhow::anyhow!("json expected to be array"));
         }
 
-        let mut current_state = None;
+        let mut initial_state = None;
         let mut states = HashMap::new();
-        for (state_name, state_data) in json.entries() {
-            let state_name = state_name.to_string();
-            if current_state.is_none() {
-                current_state = Some(state_name.clone());
+        for state_data in json.members() {
+            let state_name = &state_data["name"];
+            if !state_name.is_string() {
+                anyhow::bail!("state name should be string");
             }
-            let default = state_data["default"].as_str().unwrap().to_string();
+            let state_name = state_name.as_str().unwrap().to_string();
+
+            if initial_state.is_none() {
+                initial_state = Some(state_name.clone());
+            }
+            let default_next = match state_data["default_next"].as_str() {
+                Some(s) => Some(s.to_string()),
+                None => None,
+            };
 
             let head = &state_data["head"];
             if !head.is_array() {
@@ -60,33 +72,72 @@ impl StateMachine {
             for item in body.members() {
                 let condition = &item["condition"];
                 let condition = Expression::new(condition)?;
+
                 let expressions = &item["expressions"];
                 let expressions: Result<Vec<_>, _> = expressions.members().map(|j| Expression::new(j)).collect();
                 let expressions = expressions?;
-                body_parsed.push((condition, expressions));
+
+                let state_op = &item["state op"];
+                if !state_op.is_string() {
+                    anyhow::bail!("state op is expected to be string");
+                }
+                let state_op = StateOp::from_str(state_op.as_str().unwrap())?;
+                body_parsed.push((condition, expressions, state_op));
             }
             
-            let state = State::new(default, head, body_parsed);
+            let state = State::new(default_next, head, body_parsed);
             states.insert(state_name, state);
         }
 
-        if current_state.is_none() {
+        if initial_state.is_none() {
             return Err(anyhow::anyhow!("no states defined!"));
         }
-        let current_state = current_state.unwrap();
+        let initial_state = initial_state.unwrap();
 
-        Ok(Self::new(states, current_state))
+        Ok(Self::new(states, initial_state))
+    }
+
+
+    fn get_current_state(&self) -> anyhow::Result<&State> {
+        let state_name = match self.current_state.as_ref() {
+            Some(s) => s,
+            None => { return Err(anyhow::anyhow!("no current state! state machine is ended.")); },
+        };
+
+        let state = self.states.get(state_name);
+
+        match state {
+            None => Err(anyhow::anyhow!("state {state_name} not found")),
+            Some(state) => Ok(state),
+        }
     }
 
     // Use `DeserializeOwned` instead `Deserialize` and dealing with lifetime issues
     // https://users.rust-lang.org/t/lifetime-confusion-with-function-parameter-serde-deserialize/76842
-    pub fn run<I: Serialize, O: DeserializeOwned>(&mut self, i: I) -> anyhow::Result<O> {
-        let i = serde_json::to_string(&i)?;
-        let i = json::parse(&i)?;
-        let state = self.states.get(&self.current_state).unwrap();
-        let o = state.run(&i, &mut self.globals)?;
-        let o = o.to_string();
-        let o: O = serde_json::from_str(&o)?;
-        Ok(o)
+    pub fn run<I: Serialize, O: DeserializeOwned>(&mut self, i: I) -> anyhow::Result<Option<O>> {
+        let (rv, state_op) = match &self.current_state {
+            Some(current_state) => {
+                let i = serde_json::to_string(&i)?;
+                let i = json::parse(&i)?;
+                let state = self.get_current_state()?; // might need to look at RefCell for this, need
+                                                       // immutable access to the state but don't
+                                                       // want to have to clone it
+                let (o, state_op) = state.run(&i, &mut self.globals)?;
+                let o = o.to_string();
+                let o: O = serde_json::from_str(&o)?;
+                (Some(o), state_op)
+            },
+            None => {
+                (None, StateOp::Stay)
+            }
+        };
+
+        match state_op {
+            StateOp::Stay => {},
+            StateOp::End => { self.current_state = None; },
+            StateOp::ChangeTo(state_name) => { self.current_state.insert(state_name); },
+        }
+
+        Ok(rv)
     }
 }
