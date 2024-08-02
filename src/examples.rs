a0d97e6d82808e4b31aeca2e27e1a2b68aafbab8
Chris B <cbosoft@protonmail.com>
2024-08-02 13:44:34 +0100

    Update examples
    Using new sugar and list op

--- src/examples.rs
+++ src/examples.rs
@@ -1,5 +1,7 @@
 //! # Examples
 //!
+//!
+//! Compile SML source into a [StateMachine] and run it.
 //! ```
 //! use shakemyleg::compile;
 //! use serde::{Serialize, Deserialize};
@@ -30,7 +32,7 @@
 //!     outputs.state = "first"
 //!   when inputs.bar < 10:
 //!     outputs.bar = inputs.bar + 1
-//!   when outputs.bar >= 10:
+//!   otherwise:
 //!     changeto second
 //!   
 //! state second:
@@ -38,7 +40,7 @@
 //!     outputs.state = "second"
 //!   when inputs.bar > 1:
 //!     outputs.bar = inputs.bar - 1
-//!   when outputs.bar <= 1:
+//!   otherwise:
 //!     changeto first
 //! "#;
 //!
@@ -50,4 +52,39 @@
 //! assert_eq!(o, Outputs { state: "first".to_string(), foo: 1u8, bar: 4u8 });
 //! ```
 //!
+//! We can't define a list literal in SML, but we can interact with lists passed into the machine:
+//! ```
+//! use shakemyleg::compile;
+//! use serde::{Serialize, Deserialize};
 //!
+//! #[derive(Serialize)]
+//! struct Globals {
+//!   things: Vec<String>
+//! }
+//!
+//! #[derive(Serialize)]
+//! struct Inputs {
+//!   thing: String
+//! }
+//!
+//! #[derive(Deserialize, PartialEq, Debug)]
+//! struct Outputs {
+//!   things: Vec<String>
+//! }
+//!
+//! let src = r#"
+//! state ThingAccumulator:
+//!   head:
+//!     globals.things = globals.things + inputs.thing
+//!     outputs.things = globals.things
+//!   when globals.things contains "lastthing":
+//!     end
+//! "#;
+//!
+//! let mut sm = compile(src).unwrap();
+//! sm.reinit(Globals { things: Vec::new() });
+//!
+//! let i = Inputs { thing: "FirstThing".to_string() };
+//! let o: Outputs = sm.run(i).unwrap().unwrap();
+//! assert_eq!(o, Outputs { things: vec![ "FirstThing".to_string() ] });
+//! ```
